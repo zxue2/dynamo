759e874d82e4379c95a4480a3d9f9bea67d0418a
ishandhanani <82981111+ishandhanani@users.noreply.github.com>
2025-11-20 22:55:54 -0800

    fix: set `SGLANG_BLOCK_NONZERO_RANK_CHILDREN` to be no-op  (#4527)

--- components/src/dynamo/sglang/main.py
+++ components/src/dynamo/sglang/main.py
@@ -102,8 +102,11 @@ async def init(runtime: DistributedRuntime, config: Config):
     server_args, dynamo_args = config.server_args, config.dynamo_args
 
     # Prevent SGLang from blocking on non-leader nodes
+    # We can switch this to 0 and leverage our own metrics
+    # after https://github.com/sgl-project/sglang/pull/13686
+    # is merged in
     if server_args.node_rank >= 1:
-        os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "0"
+        os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "1"
 
     engine = sgl.Engine(server_args=server_args)
 
@@ -188,8 +191,11 @@ async def init_prefill(runtime: DistributedRuntime, config: Config):
     server_args, dynamo_args = config.server_args, config.dynamo_args
 
     # Prevent SGLang from blocking on non-leader nodes
+    # We can switch this to 0 and leverage our own metrics
+    # after https://github.com/sgl-project/sglang/pull/13686
+    # is merged in
     if server_args.node_rank >= 1:
-        os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "0"
+        os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "1"
 
     engine = sgl.Engine(server_args=server_args)
 
